5daea993e5cc7aedbfaab6ddf8379b1f415cc3ad
Marek Procházka <m.prochazka2002@gmail.com>
2024-09-18 20:00:35 +0200

    base styles rework

--- app.vue
+++ app.vue
@@ -5,8 +5,8 @@
 
 <template>
   <main class="bg-gradient-to-b from-indigo-950 to-black">
-    <shared-heading>Hello</shared-heading>
-    <shared-paragraph class="test">test</shared-paragraph>
+    <h1>Hello</h1>
+    <p class="test">test</p>
     <NuxtPage />
   </main>
 </template>
