6b6a773c05bfe2710e9a8fbd83ae096d52fc4197
Marek Procházka <m.prochazka2002@gmail.com>
2024-09-19 23:23:19 +0200

    pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {},
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
@@ -11,6 +12,7 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@pinia/nuxt',
     'nuxt-easy-lightbox',
+    '@nuxt/image',
   ],
   shadcn: {
     /**
