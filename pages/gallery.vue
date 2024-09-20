cc5784e2a128fc11ad2d33cf8b7be501e8000440
Marek Procházka <m.prochazka2002@gmail.com>
2024-09-20 08:53:11 +0200

    grid?

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -17,17 +17,22 @@ const showImg = (index: number) => {
 };
 const onHide = () => (visibleRef.value = false);
 
-const randomSpan = () => [
-  `col-span-${Math.floor(Math.random() * 6) + 1}`,
-  `row-span-${Math.floor(Math.random() * 2) + 1}`,
-];
+// const randomSpan = () => [
+//   `col-span-${Math.floor(Math.random() * 4) + 1}`,
+//   `row-span-${Math.floor(Math.random() * 2) + 1}`,
+// ];
+
+const randomSpanStyle = () => ({
+  gridColumn: `span ${Math.floor(Math.random() * 4) + 1}`,
+  gridRow: `span ${Math.floor(Math.random() * 2) + 1}`,
+});
 </script>
 
 <template>
   <div class="h-full">
     <shared-go-home />
     <ScrollArea class="h-5/6 w-full">
-      <div class="grid grid-cols-12 gap-2">
+      <div class="grid grid-cols-10 gap-2">
         <div
           v-for="(src, index) in images"
           :key="index"
@@ -36,10 +41,11 @@ const randomSpan = () => [
             'max-w-full',
             'rounded-lg',
             'flex',
-            ...randomSpan(),
+            // ...randomSpan(),
             // 'row-span-2',
             // 'col-span-3',
           ]"
+          :style="randomSpanStyle()"
           @click="() => showImg(index)"
         >
           <img class="h-full w-full object-cover" :src="src.src" >
